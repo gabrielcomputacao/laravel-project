c58e8c41dcf40944b95be79df92f3b0c03b1ff56
gabriel <gabrielcomputacao@outlook.com>
2023-06-04 21:37:50 -0300

    feat: populando o banco com seed e criando factory

---
+++ laravel-steps/database/factories/PessoaFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pessoa>
+ */
+class PessoaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "nome" => $this->faker->name(),
+            "email" => $this->faker->email(),
+            "idade" => $this->faker->numberBetween(),
+        ];
+    }
+}
